3a8760ae2b806d83550e8abb25e42e85118383ff
rebelyer <wzygmuntowicz.zygmuntowicz@gmail.com>
2015-08-21 03:31:05 +0200

    Created migration files

--- db/migrate/20150820224843_create_reservations.rb
+++ db/migrate/20150820224843_create_reservations.rb
@@ -1,6 +1,14 @@
 class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
+      t.belongs_to :client, index: true
+      t.belongs_to :room, index: true
+      t.integer :adults_number
+      t.integer :children_number
+      t.date :beginning
+      t.date :end
+      t.datetime :reservation_date
+      t.decimal :discount, precision: 2, scale: 2
 
       t.timestamps null: false
     end
--- db/migrate/20150820230528_create_clients.rb
+++ db/migrate/20150820230528_create_clients.rb
@@ -1,6 +1,10 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
+      t.string :name
+      t.string :surname
+      t.integer :phone_number
+      t.string :email_adress
 
       t.timestamps null: false
     end
