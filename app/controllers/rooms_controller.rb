3eb19ca1c95bc11df89bb9c5158ce540c0dc9162
rebelyer <wzygmuntowicz.zygmuntowicz@gmail.com>
2015-08-23 19:04:42 +0200

    Added some room actions

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,25 @@
 class RoomsController < ApplicationController
-  def index
-    @rooms = Room.all
-  end
+   def index
+      @rooms = Room.all
+   end
+
+   def new
+      @room = Room.new
+   end
+
+   def create
+      @room = Room.new(room_params)
+
+      if @room.save
+         redirect_to rooms_url
+      else
+         render 'new'
+      end
+   end
+
+private
+   def room_params
+      params.require(:room).permit(:room_number, :beds_number, :price_per_adult, :price_per_child)
+   end
+
 end
