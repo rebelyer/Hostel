13af1c4b83a00cfabce434af914622e577f82b96
rebelyer <wzygmuntowicz.zygmuntowicz@gmail.com>
2015-10-15 21:33:46 +0200

    Added authentication with sorcery gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+# Use Sorcery for authentication
+gem 'sorcery'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006113850) do
+ActiveRecord::Schema.define(version: 20151015182347) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "first_name"
@@ -38,5 +38,15 @@ ActiveRecord::Schema.define(version: 20151006113850) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
